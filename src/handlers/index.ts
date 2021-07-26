6b902c8a9800a6aaaea91ab83985d73860423d1b
Vyvy-vi <vyom.j@protonmail.com>
2021-07-26 20:24:02 +0530

    remove save-wallet handler

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -4,11 +4,8 @@ import help from "./help";
 import brightid from "./brightid";
 import price from "./price";
 import metrics from "./getMetrics";
-//const { verifyDiscourse, checkDiscourse } from './discourse')
-//const { verifyGithub, checkGithub } from './github')
 import official from "./getOfficialAccounts";
 import xDai from "./getxDai";
-import saveWallet from "./saveWallet";
 import ayuda from "./ayuda";
 import sites from "./sites";
 
@@ -18,12 +15,7 @@ const handlers = new Map([
   ["brightid", brightid],
   ["price", price],
   ["metrics", metrics],
-  //['verify-discourse', verifyDiscourse],
-  //['check-discourse', checkDiscourse],
-  //['verify-github', verifyGithub],
-  //['check-github', checkGithub],
   ["sites", sites],
-  ["save-wallet", saveWallet],
   ["official", official],
   ["network", xDai],
   ["ayuda", ayuda],
